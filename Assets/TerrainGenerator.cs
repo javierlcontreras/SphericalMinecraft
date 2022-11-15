f30b1bc8b6b64e9141a3d9c53975b46db3f1ed16
javierlc@mit.edu <javierlc@mit.edu>
2022-11-14 18:51:59 -0800

    Cleaning up with gitignore 2

--- Assets/TerrainGenerator.cs
+++ Assets/TerrainGenerator.cs
@@ -7,7 +7,13 @@ public class TerrainGenerator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GameObject
+        Mesh mesh = new Mesh();
+
+        Vector3[] vertices = new Vector3[0];
+
+        GameObject world = new GameObject("World", typeof(MeshFilter), typeof(MeshRenderer));
+        world.GetComponent<MeshFilter>().mesh = mesh.
+
         
     }
 
